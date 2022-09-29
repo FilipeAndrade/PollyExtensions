ac8117a835f59a8ff164b42e5e393b5f8ca30544
Filipe Andrade <filipe.dsandrade@outlook.com>
2022-09-29 16:53:13 -0300

    Commit inicial

---
+++ src/PollyExtensionsSettings.cs
@@ -0,0 +1,41 @@
+﻿namespace PollyExtensions
+{
+    /// <summary>
+    /// Representação das configurações Polly
+    /// </summary>
+    public class PollyExtensionsSettings
+    {
+        /// <summary>
+        /// Quantidade de tentativas que será realizada
+        /// </summary>
+        public int RetryCount { get; set; }
+
+        /// <summary>
+        /// Tempo de espera entre cada tentativa
+        /// </summary>
+        public int RetryAttempt { get; set; }
+
+        public PollyExtensionsSettings()
+        {
+
+        }
+
+        public PollyExtensionsSettings(int retryCount, int retryAttempt)
+        {
+            RetryCount = retryCount;
+            RetryAttempt = retryAttempt;
+        }
+
+        /// <summary>
+        /// Cria uma configuração padrão quando não é informado nenhuma configuração para polly
+        /// </summary>
+        /// <returns>Configuração padrão</returns>
+        public PollyExtensionsSettings Default()
+        {
+            RetryCount = 2;
+            RetryAttempt = 20;
+
+            return this;
+        }
+    }
+}
